d8be5095d546aea8d005177160bb9b1a77353c22
karthikkun <karthik.kun009@gmail.com>
2024-06-03 11:50:20 -0400

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -14,8 +14,6 @@ int main(int argc, char **argv)
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
-	// Uncomment this block to pass the first stage
-
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0)
 	{
@@ -23,8 +21,7 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
+	// setting SO_REUSEADDR ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
 	{
@@ -40,6 +37,7 @@ int main(int argc, char **argv)
 	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
 	{
 		std::cerr << "Failed to bind to port 4221\n";
+		close(server_fd);
 		return 1;
 	}
 
@@ -47,21 +45,42 @@ int main(int argc, char **argv)
 	if (listen(server_fd, connection_backlog) != 0)
 	{
 		std::cerr << "listen failed\n";
+		close(server_fd);
 		return 1;
 	}
 
 	struct sockaddr_in client_addr;
-	int client_addr_len = sizeof(client_addr);
+	socklen_t client_addr_len = sizeof(client_addr);
 
 	std::cout << "Waiting for a client to connect...\n";
 
-	int client_sd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+	int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
 	std::cout << "Client connected\n";
 
-	std::string msg = "HTTP/1.1 200 OK\r\n\r\n";
-	send(client_sd, msg.c_str(), msg.length(), 0);
+	char buff[65536] = {};
+	ssize_t bytes_received = recv(client_fd, buff, sizeof(buff) - 1, 0);
+	if (bytes_received < 0) {
+		std::cerr << "recv failed\n";
+		close(client_fd);
+		close(server_fd);
+		return 1;
+	}
+	buff[bytes_received] = '\0';
+
+	std::string response_200 = "HTTP/1.1 200 OK\r\n\r\n";
+	std::string response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+	
+	// parsing url target
+	std::string request(buff);
+    int start = request.find(" ") + 1;
+    int end = request.find(" ", start);
+    if (request.substr(start, end - start) == "/")
+      send(client_fd, response_200.c_str(), response_200.length(), 0);
+    else
+      send(client_fd, response_404.c_str(), response_404.length(), 0);
 
 	close(server_fd);
+	close(client_fd);
 
 	return 0;
 }
